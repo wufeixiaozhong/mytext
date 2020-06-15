b4796789c3a72408cbfbb1d539a9d94592dfe2c2
@wang_chao <1727084024@qq.com>
2020-06-15 14:52:53 +0800

    登录模块-路由配置

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login">
+      登录页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+  data () {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+  <style lang="less" scoped>
+
+  </style>
