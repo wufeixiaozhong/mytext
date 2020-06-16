665d8d4f9cb39db2b4aef0cfd942b643a6f5c803
@wang_chao <1727084024@qq.com>
2020-06-16 12:55:05 +0800

    首页模块-配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/login'
+import Home from '../views/home'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
   // {
   //   path: '/about',
---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">首页</div>
+</template>
+
+<script>
+export default {
+  name: 'layout',
+  data () {
+    return {
+
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
